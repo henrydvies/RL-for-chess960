ed42ce3b9a8f601109e422fe17dffc355343faef
henrydvies <85992819+henrydvies@users.noreply.github.com>
2026-06-04 15:17:50 +0100

    Feat: Basic class for chess gym

---
+++ game/board_representation.py
@@ -0,0 +1,3 @@
+"""
+Tensor converstion: 8*8*12, as 8*8 board, 6 pieces * 2 colours = 12 total layers to represent pieces
+"""
---
+++ game/environment.py
@@ -0,0 +1,20 @@
+"""
+Wrapper for python-chess representing the gym environment
+"""
+import chess
+import gymnasium as gym
+
+
+class ChessEnvironment(gym.Env):
+    """
+    Class to represent chess gym environment, used for training. 
+    """
+
+    def __init__(self):
+        pass
+    
+    def reset(self):
+        pass
+    
+    def step(self):
+        pass
